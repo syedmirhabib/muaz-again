1165db6bb58b05a55a95c386809292271fd09848
Syed Mir Habib <mirakik.habib@gmail.com>
2024-01-03 16:35:56 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: isProd ? "https://renebitter.github.io/" : "",
+  },
+  basePath: "/portfolio",
+  presets: ["next/babel"],
+};
+
+module.exports = nextConfig;
